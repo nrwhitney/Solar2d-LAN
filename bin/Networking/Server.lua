89fa63754e5bfcc0743c664181763c629a3dee1f
nrwhitney <139629096+nrwhitney@users.noreply.github.com>
2023-10-10 18:47:27 +0100

    Update Server.lua
    As per readme.

--- bin/Networking/Server.lua
+++ bin/Networking/Server.lua
@@ -1,5 +1,30 @@
 local socket = require( "socket" )
-local JSON = require("json")
+--local JSON = require("json")
+
+local isSocket2 = (string.match( require("socket")._VERSION, "LuaSocket 2" ) ~= nil )
+
+local forceIPV4 	= true
+--local forceIPV4 	= false
+
+local socketUDP 	= (isSocket2) and socket.udp or(socket.udp6 or socket.udp4 or socket.udp)
+local socketTCP 	= (isSocket2) and socket.tcp or(socket.tcp6 or socket.tcp4 or socket.tcp)
+
+if( forceIPV4 and isSocket2 == false ) then
+  socketUDP 	= socket.udp4 or socket.udp
+  socketTCP 	= socket.tcp4 or socket.tcp
+end
+
+-- If we are using Socket 3, we still need to determine if the local network is
+-- ipv4 or ipv6 compliant.  Assume it is, but test for failure
+--local ipv6Test = true
+--if( isSocket2 == false ) then
+--  local socketTest1 = socket.udp6()
+--  socketTest1:setpeername( "google.com", 54613 )
+--  local testPeerIP = socketTest1:getsockname()
+--  if (testPeerIP == "::") then
+--    ipv6Test = false
+--  end
+--end
 
 local function getIP()
     local s = socket.udp()
@@ -44,30 +69,31 @@ local function BuildFunction(Function, Args)
     else
         builder = builder..Args
     end
+    --print("builder:", builder)
     return builder
 end
 
 Server = {}
-
     Server.Name = ""
     Server.AdvertiseSock = nil
     Server.StartAdvertise = function( serverName, port )
         Server.Name = serverName
         if Server.Advertisebroadcaster == nil then
-            local msg = "M1sT0xS3rv3r"
-
-            Server.AdvertiseSock = socket.udp()
-            if system.getInfo( "environment" ) == "simulator" then -- if isEditor then
-                assert(Server.AdvertiseSock:setpeername(getIP(), port))
-            else
-                assert(Server.AdvertiseSock:setpeername("239.1.1.234", port))
-            end
-            Server.AdvertiseSock:settimeout(0)
+            local msg = "g00pS3rv3r"
+            
+            local UDPBroadcaster = socketUDP()
+            UDPBroadcaster:setsockname("*", 0) --bind on any availible port and localserver ip address.
+            UDPBroadcaster:settimeout(0)
+            Server.AdvertiseSock = UDPBroadcaster
 
             local function broadcast()
-                assert(Server.AdvertiseSock:send( msg..Server.Name ))
+                
+                Server.AdvertiseSock:setoption( "broadcast", true )  --turn on broadcast
+                Server.AdvertiseSock:sendto(msg..Server.Name, "255.255.255.255", port)
+                Server.AdvertiseSock:setoption( "broadcast", false )  --turn off broadcast
+
             end
-         
+
             Server.Advertisebroadcaster = timer.performWithDelay( 100, broadcast, 0 )
         else
             error("The server is already broadcasting")
@@ -75,17 +101,20 @@ Server = {}
     end
 
     Server.StopAdvertise = function()
+      if Server.Advertisebroadcaster then
         timer.cancel( Server.Advertisebroadcaster )
         Server.Advertisebroadcaster = nil
-        if Server.AdvertiseSock ~= nil then
-            Server.AdvertiseSock:close()
-            Server.AdvertiseSock = nil
-        end
+      end
+      if Server.AdvertiseSock ~= nil then
+          Server.AdvertiseSock:close()
+          Server.AdvertiseSock = nil
+      end
     end
 
     Server.StartServer = function( UserName, Port )
-        local tcp, err = socket.bind( getIP(), Port )
-
+        local tcp = socketTCP()
+        tcp:bind( getIP(), Port )
+        tcp:listen(0)
         local Ctrl = {}
         Ctrl.UID = GUID()
         Ctrl.IsHost = true
@@ -152,10 +181,10 @@ Server = {}
         local function sPulse()
             repeat
                 local client = Ctrl.serverSock:accept()
+                
                 if client then
-                    client:settimeout( 0 )  
+                    client:settimeout( 0 )
                     table.insert( Ctrl.clientList, client )
-
                     local ID = GUID()
                     Ctrl.clientBuffer[ID] = {}
                     Ctrl.clientBuffer[ID]["UID"] = ID
@@ -180,20 +209,42 @@ Server = {}
                         for i, thisData in ipairs( receiveBuffer ) do
                             ---------------------------------------------------------------------------
                             local splitPacket = Split(thisData, "|")
-
                             local FromUID = from(client)["UID"]     -- Who the packet is sent from
                             local ToUID = splitPacket[1]            -- Who the packet is sent to
                             local Funct = splitPacket[2]            -- Function attached to packet
                             local Args = Split(splitPacket[3], "/") -- Arguments attatched to packet
-
                             local x = find(ToUID) -- If is a username returns UID if not then null
                             if x then -- if username found
                                 ToUID = x -- set UID from username to UID
                             end
-
                             if Funct == "SetUserName" then
                                 Ctrl.clientBuffer[FromUID]["UserName"] = Args
-                                if Ctrl.onReceive ~= nil then Ctrl.onReceive( "UserJoined", Args, Args ) end
+--                                if Ctrl.onReceive ~= nil then Ctrl.onReceive( "UserJoined", Args, Args ) end
+                                if Ctrl.onReceive ~= nil then Ctrl.onReceive( "UserJoined", Args, FromUID ) end
+
+
+--                            elseif Funct == "Ping" then
+
+--                                print(system.getTimer())
+--                              if Ctrl.onReceive ~= nil then Ctrl.onReceive( "Ping", Args, FromUID ) end
+--                              local data, err = Ctrl.clientBuffer[FromUID]["Socket"]:send( "Ping|"..FromUID.."\n" )
+
+                            elseif Funct == "RemoveClient" then
+                                local tcpClient = Ctrl.clientBuffer[FromUID]["Socket"]
+                                --local userName = Ctrl.clientBuffer[FromUID]["UserName"]
+
+                                --print("REMOVING")
+
+                                for i, c in pairs( Ctrl.clientList ) do
+                                  if c == tcpClient then
+                                    table.remove(Ctrl.clientList, i)
+                                    if Ctrl.onReceive ~= nil then Ctrl.onReceive( "RemoveClient", i+1, FromUID ) end
+                                  end
+                                end
+                                Ctrl.clientBuffer[FromUID] = nil
+                                local data, err = tcpClient:send( "RemoveClient|"..FromUID.."\n" )
+                                timer.performWithDelay(500, function() tcpClient:close() end )
+ 
                             else
                                 if ToUID == "." then -- The Send All tag
                                     for i,v in pairs(Ctrl.clientBuffer) do
@@ -203,8 +254,10 @@ Server = {}
                                     end
                                     if Ctrl.onReceive ~= nil then Ctrl.onReceive( Funct, Args, Ctrl.clientBuffer[FromUID]["UserName"] ) end
                                 else
-                                    if UID == Ctrl.UID then
-                                        if Ctrl.onReceive ~= nil then Ctrl.onReceive( Funct, Args, Ctrl.clientBuffer[FromUID]["UserName"] ) end
+--                                    if UID == Ctrl.UID then
+                                    if ToUID == Ctrl.UID then
+--                                        if Ctrl.onReceive ~= nil then Ctrl.onReceive( Funct, Args, Ctrl.clientBuffer[FromUID]["UserName"] ) end
+                                        if Ctrl.onReceive ~= nil then Ctrl.onReceive( Funct, Args, Ctrl.clientBuffer[FromUID]["UID"] ) end
                                     else
                                         table.insert( Ctrl.clientBuffer[UID]["SendBuffer"], BuildFunction(Funct, Args) )
                                     end
@@ -217,6 +270,7 @@ Server = {}
                 -- Send to the client
                 for ID, Connection in pairs( Ctrl.clientBuffer ) do
                     for i, msg in pairs( Connection["SendBuffer"] ) do
+                        -- print("--- Sending: ", i, msg, "---")
                         local data, err = Connection["Socket"]:send( msg )
                     end
                     Connection["SendBuffer"] = {}
@@ -224,8 +278,8 @@ Server = {}
             end
         end
      
-        Ctrl.serverBroadcaster = timer.performWithDelay( 100, sPulse, 0 )
-
+        Ctrl.serverBroadcaster = timer.performWithDelay( 20, sPulse, 0 )
+--        Ctrl.serverBroadcaster = timer.performWithDelay( 100, sPulse, 0 )
         return Ctrl
     end
  
